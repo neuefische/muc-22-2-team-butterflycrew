4c52fff986fad616af682d33a7ced5f513945597
Marcus <marcus.schweizer91@gmail.com>
2022-11-24 17:32:39 +0100

    Update annotation

--- backend/src/main/java/de/neuefische/backend/service/IDService.java
+++ backend/src/main/java/de/neuefische/backend/service/IDService.java
@@ -1,7 +1,10 @@
 package de.neuefische.backend.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.UUID;
 
+@Service
 public class IDService {
 
     public String generateID(){
