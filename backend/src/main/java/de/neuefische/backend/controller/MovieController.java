bfe371f3982c92b35e255c29b5c9708deb0c31f5
Marcus <marcus.schweizer91@gmail.com>
2022-11-24 17:51:07 +0100

    Add MovieController

---
+++ backend/src/main/java/de/neuefische/backend/controller/MovieController.java
@@ -0,0 +1,54 @@
+package de.neuefische.backend.controller;
+
+
+import de.neuefische.backend.models.Movie;
+import de.neuefische.backend.repo.MovieRepo;
+import de.neuefische.backend.service.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movies")
+
+public class MovieController {
+
+    private final MovieService movieService;
+
+
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies(){
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieByID(@PathVariable String id){
+        return movieService.getMovieByID(id);
+    }
+
+    @PostMapping
+    public Movie addMovie(@RequestBody Movie movieToAdd){
+        return movieService.addMovie(movieToAdd);
+    }
+
+    @PutMapping(path = "/{id}/update, {id}")
+    public Movie updateMovie (@PathVariable String id, @RequestBody Movie movieToUpdate){
+        if(!movieToUpdate.id().equals(id)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The ID in the URL does not match the request body's ID");
+        }
+        return movieService.updateMovie(movieToUpdate);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable String id){
+        movieService.deleteMovie(id);
+    }
+
+}
