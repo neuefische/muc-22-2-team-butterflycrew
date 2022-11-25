bc48264c73fd793764c4edcd235c241087108a87
devBK84 <bk@kharashfeh.com>
2022-11-25 15:36:08 +0100

    Add ModelMovie & Genre

---
+++ frontend/src/MovieGallery/model/Movie.ts
@@ -0,0 +1,14 @@
+import {Genre} from "./Genre";
+
+export type Movie = {
+    id: string,
+    imdb_id: string,
+    title: string,
+    genres: Genre[],
+    release_date: string
+    status: string
+    budget: number,
+    overview: string,
+    runtime: number,
+    vote_average: number
+}
