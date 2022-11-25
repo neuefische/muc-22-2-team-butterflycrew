b0414a9529d6c6dfdbfb6f3cb45c62e1a086c77c
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-11-25 17:42:50 +0100

    Update return

--- frontend/src/MovieGallery/component/Details.tsx
+++ frontend/src/MovieGallery/component/Details.tsx
@@ -2,11 +2,25 @@ import {useEffect, useState} from "react";
 import {Movie} from "../model/Movie";
 import {useParams} from "react-router-dom";
 import axios from "axios";
+import {getImage} from "../api/image-api";
 
 export default function Details(){
 
+    const mtMovie: Movie = {
+        id: "",
+        imdb_id: "",
+        title: "",
+        genres: [],
+        release_date: "",
+        status: "",
+        budget: 0,
+        overview: "",
+        runtime: 0,
+        vote_average: 0,
+        poster_path: ""
+    }
     const params = useParams()
-    const [movie, setMovie] = useState<Movie>()
+    const [movie, setMovie] = useState<Movie>(mtMovie)
 
     const id: string | undefined = params.id
 
@@ -29,24 +43,25 @@ export default function Details(){
        return movie.genres.map(genre =>{
             return <div>{genre.name + ", "} </div>
         })
-
     }
 
     return(
         <div>
             {!movie && <p>loading files...</p>}
-            {movie &&   <div>
-                            <h1>Name: {movie.title}</h1>
-                            <p>imbd ID: {movie.imdb_id}</p><br/>
-                            {/*<img src={movie.image}/><br/>*/}
-                            <p>Genre: {getGenre()}</p><br/>
-                            <p>Release: {movie.release_date}</p><br/>
-                            <p>Status: {movie.status}</p><br/>
-                            <p>Budget: {movie.budget}</p><br/>
-                            <article>Overview: {movie.overview}</article><br/>
-                            <p>Runtime: {movie.runtime}</p><br/>
-                            <p>Average Rating: {movie.vote_average}</p><br/>
-                        </div>}
+            {movie &&
+                <div>
+                <h1>Name: {movie.title}</h1>
+                <p>imdb ID: {movie.imdb_id}</p><br/>
+                <img alt={movie.title + "pic"} src={getImage(movie.poster_path)}/>
+                <p>Genre: {getGenre()}</p><br/>
+                <p>Release: {movie.release_date}</p><br/>
+                <p>Status: {movie.status}</p><br/>
+                <p>Budget: {movie.budget}</p><br/>
+                <article>Overview: {movie.overview}</article><br/>
+                <p>Runtime: {movie.runtime}</p><br/>
+                <p>Average Rating: {movie.vote_average}</p><br/>
+                </div>
+            }
         </div>
     )
 }
