6f7d9ff34072c6bd30e98fbb71d5e6aa24fd3790
Marcus <marcus.schweizer91@gmail.com>
2022-11-25 18:25:38 +0100

    Add MovieGallery

---
+++ frontend/src/MovieGallery/component/MovieGallery.tsx
@@ -0,0 +1,23 @@
+import {Movie} from "../model/Movie";
+import MovieCard from "./MovieCard";
+
+type MovieGalleryProps = {
+    movieList : Movie[]
+}
+
+
+
+export default function MovieGallery(props: MovieGalleryProps){
+
+    const movieComponents = props.movieList.map((movie)=>{
+        return <MovieCard movie={movie} key={movie.id}/>
+    })
+
+    return(
+        <div>
+            <div className={"gallery"}>
+                {movieComponents}
+            </div>
+        </div>
+    )
+}
