ac9e4d8b6b8bdf3f693f18de072ce15e525ffd1a
Marcus <marcus.schweizer91@gmail.com>
2022-11-25 10:14:19 +0100

    Add JUnit Tests for Service

---
+++ backend/src/test/java/de/neuefische/backend/service/MovieServiceTest.java
@@ -0,0 +1,116 @@
+package de.neuefische.backend.service;
+
+import de.neuefische.backend.models.Movie;
+import de.neuefische.backend.repo.MovieRepo;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class MovieServiceTest {
+
+    MovieRepo movieRepo = mock(MovieRepo.class);
+    IDService idService = mock(IDService.class);
+
+    MovieService movieService = new MovieService(movieRepo, idService);
+
+
+    @Test
+    void getAllMovies() {
+        //Given
+        List<Movie> expected = Collections.emptyList();
+
+        //When
+        when(movieRepo.getMovieList()).thenReturn(expected);
+        List<Movie> result = movieService.getAllMovies();
+
+        //Then
+        assertEquals(expected, result);
+        verify(movieRepo).getMovieList();
+
+    }
+
+    @Test
+    void addMovie() {
+        //Given
+        Movie expectedMovie = new Movie(
+                "1",
+                "1234",
+                "Fight Club",
+                Collections.emptyList(),
+                "01.01.2000",
+                "released",
+                100000,
+                "BlaBlaBla",
+                129,
+                6.8 );
+        String expectedID = "1";
+
+        //When
+        when(movieRepo.addMovie(any())).thenReturn(expectedMovie);
+        when(idService.generateID()).thenReturn(expectedID);
+
+        Movie movieActual = movieService.addMovie(expectedMovie);
+
+        //Then
+        assertEquals(expectedMovie,movieActual);
+    }
+
+    @Test
+    void getMovieByID() {
+        //Given
+        Movie expectedMovie = new Movie(
+                "1",
+                "1234",
+                "Fight Club",
+                Collections.emptyList(),
+                "01.01.2000",
+                "released",
+                100000,
+                "BlaBlaBla",
+                129,
+                6.8 );
+
+                String id = "1";
+        //When
+        when(movieRepo.getMovieByID(id)).thenReturn(expectedMovie);
+        Movie actualMovie = movieService.getMovieByID(id);
+
+        //Then
+        assertEquals(expectedMovie, actualMovie);
+        verify(movieRepo).getMovieByID(id);
+    }
+
+
+
+    @Test
+    void updateMovie() {
+        //Given
+        Movie expectedMovie = new Movie(
+                "1",
+                "1234",
+                "Fight Club",
+                Collections.emptyList(),
+                "01.01.2000",
+                "released",
+                100000,
+                "BlaBlaBla",
+                129,
+                6.8 );
+
+
+
+        //When
+        when(movieRepo.updateMovie(expectedMovie)).thenReturn(expectedMovie);
+        Movie actualMovie = movieService.updateMovie(expectedMovie);
+
+        //Then
+        assertEquals(expectedMovie, actualMovie);
+        verify(movieRepo).updateMovie(expectedMovie);
+
+    }
+}
