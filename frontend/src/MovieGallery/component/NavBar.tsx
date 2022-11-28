95cfc9e47c04081f5dabd587f6b0aeafe2653c31
devBK84 <bk@kharashfeh.com>
2022-11-28 15:43:32 +0100

    Update Navbar & Create Routes in App

--- frontend/src/MovieGallery/component/NavBar.tsx
+++ frontend/src/MovieGallery/component/NavBar.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
+import logo from "../img/1.png"
+import "./NavBar.css"
+
 export default function NavBar() {
 
     return (
         <nav className={"navbar"}>
             <div className={"logo"}>
-             {/*<img onClick={...}/>   */}
-            <span>Logo</span>
+                <img src={logo} alt = 'logo'/>
+                <span>Logo</span>
             </div>
             <div>
+
                 <NavLink className={"NavLink"} to={"/"}>Home</NavLink>
                 <NavLink className={"NavLink"} to={"/movies"}>Character</NavLink>
             </div>
         </nav>
-)
+    )
 
 }
 
