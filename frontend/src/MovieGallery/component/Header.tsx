f26711af1d8bab8f4a7cec2beb14fbdbec16ca82
Marcus <marcus.schweizer91@gmail.com>
2022-11-28 16:56:24 +0100

    Add Header

---
+++ frontend/src/MovieGallery/component/Header.tsx
@@ -0,0 +1,12 @@
+import NavBar from "./NavBar";
+
+export default function Header(){
+
+    return(
+        <div>
+            <NavBar/>
+            <h1>Welcome.</h1>
+            <h2>Discover millions of movies...</h2>
+        </div>
+    )
+}
