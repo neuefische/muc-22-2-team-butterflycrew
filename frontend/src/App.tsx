7d17b9db550660bb4af5f199476d72f88c798c62
Marcus <marcus.schweizer91@gmail.com>
2022-11-28 18:21:40 +0100

    Add Header to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import NavBar from "./MovieGallery/component/NavBar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Details from "./MovieGallery/component/Details";
+import Header from "./MovieGallery/component/Header";
 
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-            <NavBar/>
+            <Header/>
 
             <Routes>
                 <Route path={"/"} element={<p>"Movies"</p>}></Route>
