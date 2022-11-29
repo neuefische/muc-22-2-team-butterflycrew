3d6f5f5e6f5f790b141187387a8c6bb18aa3288c
devBK84 <bk@kharashfeh.com>
2022-11-29 18:55:25 +0100

    Update Logo

--- frontend/src/MovieGallery/component/NavBar.tsx
+++ frontend/src/MovieGallery/component/NavBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
-import logo from "../img/1.png"
+import logo from "../img/ Butterfly_ohne_Schrift.png"
 import "./NavBar.css"
 
 export default function NavBar() {
@@ -9,12 +9,9 @@ export default function NavBar() {
         <nav className={"navbar"}>
             <div className={"logo"}>
                 <img src={logo} alt = 'logo'/>
-                <span>Logo</span>
-            </div>
-            <div>
-
                 <NavLink className={"NavLink"} to={"/"}>Home</NavLink>
-                <NavLink className={"NavLink"} to={"/movies"}>Character</NavLink>
+                <NavLink className={"NavLink"} to={"/movies"}>Movies</NavLink>
+
             </div>
         </nav>
     )
