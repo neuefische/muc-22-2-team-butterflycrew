2af02bb12c7adb1a987772ab4653b26f69089cbe
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-11-29 13:21:57 +0100

    Add UpcomingMovieGallery to Home

---
+++ frontend/src/MovieGallery/component/Home.tsx
@@ -0,0 +1,27 @@
+import UpcomingMovieGallery from "./UpcomingMovieGallery";
+import {useEffect, useState} from "react";
+import {UpcomingMovie} from "../model/UpcomingMovie";
+import axios from "axios";
+
+export default function Home(){
+
+    const [upcomingMovies, setUpcomingMovies] = useState<UpcomingMovie[]>([])
+
+    useEffect(() => {
+        getUpcomingMovies()
+    }, [])
+
+    function getUpcomingMovies(){
+        axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=541894e58c6ae639dc89b9c8e73a5cf8&language=de")
+            .then((response) => {
+                setUpcomingMovies(response.data.results)
+            })
+            .catch(e => console.error(e))
+    }
+
+    return(
+        <div>
+            <UpcomingMovieGallery upcomingMovies={upcomingMovies} />
+        </div>
+    )
+}
