db12a808f2f875b44659f070af095e6fda589eb2
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-11-29 13:19:37 +0100

    Update baseUrl

--- frontend/src/MovieGallery/api/image-api.ts
+++ frontend/src/MovieGallery/api/image-api.ts
@@ -1,6 +1,6 @@
 import React from "react";
 
-const baseUrl:string = "https://image.tmdb.org/t/p"
+const baseUrl:string = "https://image.tmdb.org/t/p/w500"
 export const getImage = (poster_path: string): string => {
     return baseUrl + poster_path
 }
