69f14b96a64c6fa5a8e40152738081517ab1ae30
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-11-29 13:29:16 +0100

    Update home & add header h1 for Upcoming Movies

--- frontend/src/MovieGallery/component/Home.tsx
+++ frontend/src/MovieGallery/component/Home.tsx
@@ -21,6 +21,7 @@ export default function Home(){
 
     return(
         <div>
+            <h1>Upcoming Movies</h1>
             <UpcomingMovieGallery upcomingMovies={upcomingMovies} />
         </div>
     )
