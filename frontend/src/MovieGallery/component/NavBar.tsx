7b7b3f590f8bd11f2b83254d47bb222ee3ef4d49
devBK84 <bk@kharashfeh.com>
2022-11-30 17:45:32 +0100

    Add new font & update NavBar.css

--- frontend/src/MovieGallery/component/NavBar.tsx
+++ frontend/src/MovieGallery/component/NavBar.tsx
@@ -3,11 +3,15 @@ import {NavLink} from "react-router-dom";
 import logo from "../img/ Butterfly_ohne_Schrift.png"
 import "./NavBar.css"
 
+
 export default function NavBar() {
+
 
     return (
         <nav className={"navbar"}>
+
             <div className={"logo"}>
+                <h1 className={"logoname"}>Butterfly Movies</h1>
                 <img src={logo} alt = 'logo'/>
 
                 <div className={"navbar"}>
