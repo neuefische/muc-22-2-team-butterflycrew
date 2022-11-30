45b9c8b70b8801c0aa2f62aadab1603a73558383
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-11-30 08:52:13 +0100

    Add get function for popular videos

--- frontend/src/MovieGallery/component/Home.tsx
+++ frontend/src/MovieGallery/component/Home.tsx
@@ -9,10 +9,12 @@ export default function Home(){
 
     const [upcomingMovies, setUpcomingMovies] = useState<UpcomingMovie[]>([])
     const [topRatedMovies, setTopRatedMovies] = useState<TopRatedMovie[]>([])
+    const [popularMovies, setPopularMovies] = useState<PopularMovie[]>([])
 
     useEffect(() => {
         getUpcomingMovies()
         getTopRatedMovies()
+        getPopularMovies()
     }, [])
 
     function getUpcomingMovies(){
@@ -31,12 +33,21 @@ export default function Home(){
             .catch(e => console.error(e))
     }
 
+    function getPopularMovies(){
+        axios.get("https://api.themoviedb.org/3/movie/popular?api_key=541894e58c6ae639dc89b9c8e73a5cf8&language=de")
+            .then((response) => {
+                setPopularMovies(response.data.results)
+            })
+            .catch(e => console.error(e))
+    }
+
     return(
         <div className={"container-xl"}>
             <h1>Upcoming Movies</h1>
             <UpcomingMovieGallery upcomingMovies={upcomingMovies} />
             <h1>Top Rated Movies</h1>
             <TopRatedMovieGallery topRatedMovies={topRatedMovies} />
+            <h1>Popular Movies</h1>
         </div>
     )
 }
