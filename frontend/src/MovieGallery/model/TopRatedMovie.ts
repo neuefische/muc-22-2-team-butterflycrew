a6003567e2ef5acada77a19c90b070330b14cf3f
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-11-30 08:34:04 +0100

    Add TopRatedMovie model

---
+++ frontend/src/MovieGallery/model/TopRatedMovie.ts
@@ -0,0 +1,14 @@
+export type TopRatedMovie = {
+    poster_path: string,
+    adult: boolean,
+    overview: string,
+    genre_ids: number[],
+    id: number,
+    original_title: string,
+    original_language: string,
+    title: string,
+    backdrop_path: string,
+    popularity: number,
+    vote_count: number,
+    vote_average: number
+}
