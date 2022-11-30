9ddf26267e949522fc07d5ca6b77a2a0144d3824
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-11-30 09:02:22 +0100

    Add key to component

--- frontend/src/MovieGallery/component/PopularMovieGallery.tsx
+++ frontend/src/MovieGallery/component/PopularMovieGallery.tsx
@@ -1,5 +1,4 @@
 import {PopularMovie} from "../model/PopularMovie";
-import TopRatedMovieComponent from "./TopRatedMovieComponent";
 import PopularMovieComponent from "./PopularMovieComponent";
 
 type PopularMovieGalleryProps = {
@@ -9,7 +8,7 @@ type PopularMovieGalleryProps = {
 export default function PopularMovieGallery(props: PopularMovieGalleryProps){
     const popularMovieComponent = props.popularMovies.slice(0, 5).map((movie) => {
             return(
-                <PopularMovieComponent popularMovie={movie} />
+                <PopularMovieComponent popularMovie={movie} key={movie.id} />
             )
         })
 
