d1de72116079c3764d289e472d34ef4b9a690870
Marcus <marcus.schweizer91@gmail.com>
2022-12-01 12:42:00 +0100

    Add ExchangeController

---
+++ backend/src/main/java/de/neuefische/backend/controller/ExchangeController.java
@@ -0,0 +1,44 @@
+package de.neuefische.backend.controller;
+
+import de.neuefische.backend.models.MovieExchangeDTO;
+import de.neuefische.backend.models.MovieToExchange;
+import de.neuefische.backend.service.ExchangeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/exchange")
+public class ExchangeController {
+
+    private final ExchangeService exchangeService;
+
+    public ExchangeController(ExchangeService exchangeService) {
+        this.exchangeService = exchangeService;
+    }
+
+    @GetMapping
+    public List<MovieToExchange> getAllEntries(){
+        return exchangeService.getAllEntries();
+    }
+
+    @PostMapping
+    public MovieToExchange addEntry (@RequestBody MovieExchangeDTO entry){
+        return exchangeService.addEntry(entry);
+    }
+
+    @GetMapping(path = "/{id}")
+    public MovieToExchange getEntryByID (@PathVariable String id){
+        return exchangeService.getEntryByID(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public MovieToExchange updateEntry(@PathVariable String id, @RequestBody MovieToExchange entry){
+        return exchangeService.updateEntry(entry);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteEntry (@PathVariable String id){
+        exchangeService.deleteEntryByID(id);
+    }
+}
