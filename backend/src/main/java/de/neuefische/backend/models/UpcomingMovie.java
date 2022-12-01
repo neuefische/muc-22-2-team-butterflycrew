6d5d100742abac14453bfbee05732313eac39624
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-01 11:54:57 +0100

    Add UpcomingMovie.java

---
+++ backend/src/main/java/de/neuefische/backend/models/UpcomingMovie.java
@@ -0,0 +1,14 @@
+package de.neuefische.backend.models;
+
+import java.util.List;
+
+public class UpcomingMovie extends Movie{
+
+    String release_date;
+
+    public UpcomingMovie(String id, String imdb_id, List<Integer> genres, String title, String poster_path, String release_date) {
+        super(id, imdb_id, genres, title, poster_path);
+        this.release_date=release_date;
+    }
+
+}
