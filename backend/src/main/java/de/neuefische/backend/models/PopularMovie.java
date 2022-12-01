806732c07f2a13b9151bd09b5198eba5e143102b
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-01 11:47:07 +0100

    Rename variable

--- backend/src/main/java/de/neuefische/backend/models/PopularMovie.java
+++ backend/src/main/java/de/neuefische/backend/models/PopularMovie.java
@@ -8,8 +8,8 @@ public class PopularMovie extends Movie{
     double popularity;
 
 
-    public PopularMovie(String id, String imdb_id, List<Integer> genres, String title, String poster_pat) {
-        super(id, imdb_id, genres, title, poster_pat);
+    public PopularMovie(String id, String imdb_id, List<Integer> genres, String title, String poster_path) {
+        super(id, imdb_id, genres, title, poster_path);
     }
 
     public double getPopularity() {
