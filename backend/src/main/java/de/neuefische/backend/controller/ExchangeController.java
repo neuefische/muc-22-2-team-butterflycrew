9ec9b176dfacf1b46e409ef0dfae151718764a8b
Marcus <marcus.schweizer91@gmail.com>
2022-12-01 15:17:03 +0100

    Update PutController & updateEntry in Service

--- backend/src/main/java/de/neuefische/backend/controller/ExchangeController.java
+++ backend/src/main/java/de/neuefische/backend/controller/ExchangeController.java
@@ -33,8 +33,8 @@ public class ExchangeController {
     }
 
     @PutMapping(path = "/{id}")
-    public MovieToExchange updateEntry(@PathVariable String id, @RequestBody MovieToExchange entry){
-        return exchangeService.updateEntry(entry);
+    public MovieToExchange updateEntry(@PathVariable String id, @RequestBody MovieExchangeDTO entry){
+        return exchangeService.updateEntry(id, entry);
     }
 
     @DeleteMapping(path = "/{id}")
