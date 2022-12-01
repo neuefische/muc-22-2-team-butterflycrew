5770f4679716ce20b18d6c57928dba5ff50ae563
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-01 15:59:44 +0100

    Update Movie model

--- backend/src/main/java/de/neuefische/backend/models/Movie.java
+++ backend/src/main/java/de/neuefische/backend/models/Movie.java
@@ -1,69 +1,16 @@
 package de.neuefische.backend.models;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+public record Movie(
 
-import java.util.List;
-import java.util.Objects;
+        String id,
+        String imdbID,
+        String genres,
+        String title,
+        String poster,
+        String released,
+        String runtime,
+        String plot,
+        String metascore,
+        String boxoffice
+){}
 
-@Document("movies")
-public class Movie{
-        @Id
-        String id;
-        String imdb_id;
-        List<Integer> genres;
-        String title;
-        String poster_path;
-
-        public Movie(String id, String imdb_id, List<Integer> genres, String title, String poster_path) {
-                this.id = id;
-                this.imdb_id = imdb_id;
-                this.genres = genres;
-                this.title = title;
-                this.poster_path = poster_path;
-        }
-
-        public String getId() {
-                return id;
-        }
-
-        public String getImdb_id() {
-                return imdb_id;
-        }
-
-        public List<Integer> getGenres() {
-                return genres;
-        }
-
-        public String getTitle() {
-                return title;
-        }
-
-        public String getPoster_pat() {
-                return poster_path;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-                if (this == o) return true;
-                if (o == null || getClass() != o.getClass()) return false;
-
-                Movie movie = (Movie) o;
-
-                if (!Objects.equals(id, movie.id)) return false;
-                if (!Objects.equals(imdb_id, movie.imdb_id)) return false;
-                if (!Objects.equals(genres, movie.genres)) return false;
-                if (!Objects.equals(title, movie.title)) return false;
-                return Objects.equals(poster_path, movie.poster_path);
-        }
-
-        @Override
-        public int hashCode() {
-                int result = id != null ? id.hashCode() : 0;
-                result = 31 * result + (imdb_id != null ? imdb_id.hashCode() : 0);
-                result = 31 * result + (genres != null ? genres.hashCode() : 0);
-                result = 31 * result + (title != null ? title.hashCode() : 0);
-                result = 31 * result + (poster_path != null ? poster_path.hashCode() : 0);
-                return result;
-        }
-}
