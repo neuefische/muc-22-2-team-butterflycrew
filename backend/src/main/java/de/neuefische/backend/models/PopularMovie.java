b52f0ba705564406d7e3d2cca068147f370449a1
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-01 11:52:06 +0100

    Add vote_average to Constructor

--- backend/src/main/java/de/neuefische/backend/models/PopularMovie.java
+++ backend/src/main/java/de/neuefische/backend/models/PopularMovie.java
@@ -8,8 +8,9 @@ public class PopularMovie extends Movie{
     double popularity;
 
 
-    public PopularMovie(String id, String imdb_id, List<Integer> genres, String title, String poster_path) {
+    public PopularMovie(String id, String imdb_id, List<Integer> genres, String title, String poster_path, double popularity) {
         super(id, imdb_id, genres, title, poster_path);
+        this.popularity= popularity;
     }
 
     public double getPopularity() {
