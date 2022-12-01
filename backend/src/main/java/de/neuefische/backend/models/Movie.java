469df75415f97713728fc32e42d4935517c22427
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-01 11:45:31 +0100

    Update Movie to class

--- backend/src/main/java/de/neuefische/backend/models/Movie.java
+++ backend/src/main/java/de/neuefische/backend/models/Movie.java
@@ -4,14 +4,66 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+import java.util.Objects;
 
 @Document("movies")
-public record Movie(
+public class Movie{
         @Id
-        String id,
-        String imdb_id,
-        List<Integer> genres,
-        String title,
-        String poster_path
-) {
+        String id;
+        String imdb_id;
+        List<Integer> genres;
+        String title;
+        String poster_pat;
+
+        public Movie(String id, String imdb_id, List<Integer> genres, String title, String poster_pat) {
+                this.id = id;
+                this.imdb_id = imdb_id;
+                this.genres = genres;
+                this.title = title;
+                this.poster_pat = poster_pat;
+        }
+
+        public String getId() {
+                return id;
+        }
+
+        public String getImdb_id() {
+                return imdb_id;
+        }
+
+        public List<Integer> getGenres() {
+                return genres;
+        }
+
+        public String getTitle() {
+                return title;
+        }
+
+        public String getPoster_pat() {
+                return poster_pat;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+
+                Movie movie = (Movie) o;
+
+                if (!Objects.equals(id, movie.id)) return false;
+                if (!Objects.equals(imdb_id, movie.imdb_id)) return false;
+                if (!Objects.equals(genres, movie.genres)) return false;
+                if (!Objects.equals(title, movie.title)) return false;
+                return Objects.equals(poster_pat, movie.poster_pat);
+        }
+
+        @Override
+        public int hashCode() {
+                int result = id != null ? id.hashCode() : 0;
+                result = 31 * result + (imdb_id != null ? imdb_id.hashCode() : 0);
+                result = 31 * result + (genres != null ? genres.hashCode() : 0);
+                result = 31 * result + (title != null ? title.hashCode() : 0);
+                result = 31 * result + (poster_pat != null ? poster_pat.hashCode() : 0);
+                return result;
+        }
 }
