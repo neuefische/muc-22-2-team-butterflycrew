efc913654c51bbc9c1c264b708b344487d1ec7b1
devBK84 <bk@kharashfeh.com>
2022-12-01 16:42:55 +0100

    Add Unit test addEntry

--- backend/src/test/java/de/neuefische/backend/service/MovieServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/service/MovieServiceTest.java
@@ -4,7 +4,6 @@ import de.neuefische.backend.models.Movie;
 import de.neuefische.backend.repo.MovieRepo;
 import org.junit.jupiter.api.Test;
 
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
