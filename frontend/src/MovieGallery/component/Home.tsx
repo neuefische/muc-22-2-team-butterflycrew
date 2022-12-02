0368cc14d96642ddf50b3f5b38c0a792279b6dc7
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-02 12:14:23 +0100

    Change Home.tsx for local testing & coding

--- frontend/src/MovieGallery/component/Home.tsx
+++ frontend/src/MovieGallery/component/Home.tsx
@@ -2,43 +2,21 @@ import UpcomingMovieGallery from "./UpcomingMovieGallery";
 import {useEffect, useState} from "react";
 import {UpcomingMovie} from "../model/UpcomingMovie";
 import axios from "axios";
-import TopRatedMovieGallery from "./TopRatedMovieGallery";
-import {TopRatedMovie} from "../model/TopRatedMovie";
-import {PopularMovie} from "../model/PopularMovie";
-import PopularMovieGallery from "./PopularMovieGallery";
 
 export default function Home(){
 
     const [upcomingMovies, setUpcomingMovies] = useState<UpcomingMovie[]>([])
-    const [topRatedMovies, setTopRatedMovies] = useState<TopRatedMovie[]>([])
-    const [popularMovies, setPopularMovies] = useState<PopularMovie[]>([])
+
 
     useEffect(() => {
         getUpcomingMovies()
-        getTopRatedMovies()
-        getPopularMovies()
+
     }, [])
 
     function getUpcomingMovies(){
-        axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=541894e58c6ae639dc89b9c8e73a5cf8&language=de")
+        axios.get("http://localhost:3000/api/movies/upcoming-movies")
             .then((response) => {
                 setUpcomingMovies(response.data.results)
-            })
-            .catch(e => console.error(e))
-    }
-
-    function getTopRatedMovies() {
-        axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=541894e58c6ae639dc89b9c8e73a5cf8&language=de")
-            .then((response) => {
-                setTopRatedMovies(response.data.results)
-            })
-            .catch(e => console.error(e))
-    }
-
-    function getPopularMovies(){
-        axios.get("https://api.themoviedb.org/3/movie/popular?api_key=541894e58c6ae639dc89b9c8e73a5cf8&language=de")
-            .then((response) => {
-                setPopularMovies(response.data.results)
             })
             .catch(e => console.error(e))
     }
@@ -47,10 +25,6 @@ export default function Home(){
         <div className={"container-xl"}>
             <h1>Upcoming Movies</h1>
             <UpcomingMovieGallery upcomingMovies={upcomingMovies} />
-            <h1>Top Rated Movies</h1>
-            <TopRatedMovieGallery topRatedMovies={topRatedMovies} />
-            <h1>Popular Movies</h1>
-            <PopularMovieGallery popularMovies={popularMovies} />
         </div>
     )
 }
