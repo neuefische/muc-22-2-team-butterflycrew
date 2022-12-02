3e6d35798e93f02fc37bad01554f4ce5e2a55de7
devBK84 <bk@kharashfeh.com>
2022-12-02 22:02:48 +0100

    Add Search & Update NavBar

--- frontend/src/MovieGallery/component/NavBar.tsx
+++ frontend/src/MovieGallery/component/NavBar.tsx
@@ -1,29 +1,37 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import logo from "../img/ Butterfly_ohne_Schrift.png"
+import "../font/Amsterdam.ttf"
 import "./NavBar.css"
-
 
 export default function NavBar() {
 
 
     return (
-        <nav className={"navbar"}>
+        <div>
+            <nav className={"navbar"}>
+                <div className={"logo"}>
+                    <img src={logo} alt='logo'/>
+                    <h1 className={"logoname"}>Butterfly Movies</h1>
 
-            <div className={"logo"}>
-                <h1 className={"logoname"}>Butterfly Movies</h1>
-                <img src={logo} alt = 'logo'/>
 
-                <div className={"navbar"}>
-                <NavLink className={"NavLink"} to={"/"}>Home</NavLink>
-                <NavLink className={"NavLink"} to={"/movies"}>Movies</NavLink>
+                    <div className={"navbar"}>
+                        <NavLink className={"NavLink"} to={"/"}>Home</NavLink>
+                        <NavLink className={"NavLink"} to={"/movies"}>Movies</NavLink>
+                        <NavLink className={"NavLink"} to={"/börse"}>Börse</NavLink>
+                        <form className="d-flex mx-5" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+
+                        <button className="btn btn-outline-secondary" type="submit">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
+
+                        </form>
+                    </div>
                 </div>
+            </nav>
 
-            </div>
-        </nav>
+        </div>
     )
 
 }
-
-
-
