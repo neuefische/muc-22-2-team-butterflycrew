5fc868b77c0fd74afd9593087b79eefcafcbaa61
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-02 12:21:36 +0100

    Update Method to get different Movies

--- backend/src/main/java/de/neuefische/backend/service/MovieService.java
+++ backend/src/main/java/de/neuefische/backend/service/MovieService.java
@@ -5,6 +5,7 @@ import de.neuefische.backend.models.Movie;
 import de.neuefische.backend.repo.MovieRepo;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,11 @@ import java.util.Optional;
 public class MovieService {
 
     private final MovieRepo movieRepo;
+    private static final double TOPRATED_VALUE_TO_SHOW = 8.6;
+    private static final double POPULARE_VALUE_TO_SHOW = 4000;
+    private static final int YEAR_VALUE_TO_SHOW = 2022;
+    private static final int MONTH_VALUE_TO_SHOW = 12;
+
 
     public MovieService(MovieRepo movieRepo){
         this.movieRepo = movieRepo;
@@ -33,15 +39,39 @@ public class MovieService {
         throw new IllegalArgumentException("Id not found!");
     }
 
-    public void deleteMovieById(String id){
-        movieRepo.deleteById(id);
-    }
-
     public Movie updateMovie(Movie movieToUpdate){
         return movieRepo.save(movieToUpdate);
     }
 
+    public List<Movie> getUpcomingMovies(){
 
-
-
+        List<Movie> filteredMovies = new ArrayList<>();
+        for (Movie allMovie : getAllMovies()) {
+            String[] splitter = allMovie.release_date().split("-");
+            int year = Integer.parseInt(splitter[0]);
+            int month = Integer.parseInt(splitter[1]);
+            if (year== YEAR_VALUE_TO_SHOW &&month== MONTH_VALUE_TO_SHOW){
+                filteredMovies.add(allMovie);
+            }
+        }
+        return filteredMovies;
+    }
+    public List<Movie> getTopratedMovies(){
+        List<Movie> filteredMovies = new ArrayList<>();
+        for (Movie allMovie : getAllMovies()){
+            if (allMovie.vote_average()>= TOPRATED_VALUE_TO_SHOW){
+                filteredMovies.add(allMovie);
+            }
+        }
+        return filteredMovies;
+    }
+    public List<Movie> getPopulareMovies(){
+        List<Movie> filteredMovies = new ArrayList<>();
+        for (Movie allMovie : getAllMovies()){
+            if (allMovie.popularity()>= POPULARE_VALUE_TO_SHOW){
+                filteredMovies.add(allMovie);
+            }
+        }
+        return filteredMovies;
+    }
 }
