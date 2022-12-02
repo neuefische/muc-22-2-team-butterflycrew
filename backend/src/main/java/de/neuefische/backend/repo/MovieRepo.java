98efcdd230098e648cddb92b99b90fe1f392bb5c
VladimirKlippert93 <vladimir.klippert93@gmail.com>
2022-12-02 12:11:21 +0100

    Change MovieRepo to Interface

--- backend/src/main/java/de/neuefische/backend/repo/MovieRepo.java
+++ backend/src/main/java/de/neuefische/backend/repo/MovieRepo.java
@@ -1,11 +1,11 @@
 package de.neuefische.backend.repo;
 
 import de.neuefische.backend.models.Movie;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepo extends MongoRepository<Movie, String> {
-
 
 }
