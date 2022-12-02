09fa58db4bb1a9a556d5aea3ccd5d6c805fed101
devBK84 <bk@kharashfeh.com>
2022-12-02 22:58:41 +0100

    Add Search & Update NavBar

--- frontend/src/MovieGallery/component/NavBar.tsx
+++ frontend/src/MovieGallery/component/NavBar.tsx
@@ -6,32 +6,28 @@ import "./NavBar.css"
 
 export default function NavBar() {
 
-
     return (
         <div>
             <nav className={"navbar"}>
                 <div className={"logo"}>
                     <img src={logo} alt='logo'/>
                     <h1 className={"logoname"}>Butterfly Movies</h1>
-
-
-                    <div className={"navbar"}>
-                        <NavLink className={"NavLink"} to={"/"}>Home</NavLink>
-                        <NavLink className={"NavLink"} to={"/movies"}>Movies</NavLink>
-                        <NavLink className={"NavLink"} to={"/börse"}>Börse</NavLink>
-                        <form className="d-flex mx-5" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                </div>
+                <div className={"navigation"}>
+                    <NavLink className={"NavLink"} to={"/"}>Home</NavLink>
+                    <NavLink className={"NavLink"} to={"/movies"}>Movies</NavLink>
+                    <NavLink className={"NavLink"} to={"/börse"}>Börse</NavLink>
+                    <form className="d-flex mx-5" role="search">
+                        <input className="form-control me-2 searchbar" type="search" placeholder="Search" aria-label="Search"/>
 
                         <button className="btn btn-outline-secondary" type="submit">
                             <i className="fa-solid fa-magnifying-glass"></i>
                         </button>
 
-                        </form>
-                    </div>
+                    </form>
                 </div>
             </nav>
 
         </div>
     )
-
 }
