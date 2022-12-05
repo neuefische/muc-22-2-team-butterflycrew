1430230d391e6aaf43ff2bb9989eaf5ad5c9ca2e
MoinMarcell <marcell.dechant@protonmail.com>
2022-12-05 21:17:40 +0100

    Update MovieCard

--- frontend/src/MovieGallery/component/MovieCard.tsx
+++ frontend/src/MovieGallery/component/MovieCard.tsx
@@ -1,6 +1,9 @@
 import {Movie} from "../model/Movie";
 import {useNavigate} from "react-router-dom";
 import {getImage} from "../api/image-api";
+import * as React from "react";
+import "./MovieCard.css"
+
 export type MovieCardProps = {
     movie: Movie
 }
@@ -9,17 +12,16 @@ export default function MovieCard(props: MovieCardProps) {
 
     const navigate = useNavigate()
 
-    function onClickDetails(){
+    function handleDetailsClick(){
         navigate("/details/" + props.movie.id)
     }
 
     return (
-        <div className={"card"}>
-            <img src={getImage(props.movie.poster_path)} alt={"MoviePicture"}/>
-            <h2>{props.movie.title}</h2>
-            <p>{props.movie.release_date}</p>
-            <div className={"button"}>
-                <button onClick={onClickDetails}>Details</button>
+        <div className="col">
+            <div className="card border-0 movieCard" onClick={handleDetailsClick}>
+                <img src={getImage(props.movie.poster_path)} className="card-img-top rounded-start rounded-end" alt={props.movie.title + " Poster"} />
+                <div className={"card-img-overlay movieOverlay"}>
+                </div>
             </div>
         </div>
     )
