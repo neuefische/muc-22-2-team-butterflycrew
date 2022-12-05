b54c22541af7eecbec1fc845ca575c9bf1fdba7b
Marcus <marcus.schweizer91@gmail.com>
2022-12-05 17:29:57 +0100

    Bug Fix Header.tsx

--- frontend/src/MovieGallery/component/Header.tsx
+++ frontend/src/MovieGallery/component/Header.tsx
@@ -9,7 +9,7 @@ export default function Header(){
         <div>
             <NavBar/>
             <div className="card-width card text-bg-dark">
-                <img src={bg} className="card-img" alt="Background Picture"/>
+                <img src={bg} className="card-img" alt="Background"/>
                     <div className="card-img-overlay header1">
                         <h2 className="card-title title1">Welcome.</h2>
                         <h4 className="card-text pb-2 title2">Discover millions of movies...</h4>
