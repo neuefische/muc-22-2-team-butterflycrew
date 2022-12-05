e59865bda16d834c066d6c9d8dc42d5353ff130c
Marcus <marcus.schweizer91@gmail.com>
2022-12-05 17:19:44 +0100

    Add Header & Header.css

--- frontend/src/MovieGallery/component/Header.tsx
+++ frontend/src/MovieGallery/component/Header.tsx
@@ -1,12 +1,33 @@
 import NavBar from "./NavBar";
+import bg from '../img/StarWars.jpg'
+import "./Header.css"
+import React from "react";
 
 export default function Header(){
 
     return(
         <div>
             <NavBar/>
-            <h1>Welcome.</h1>
-            <h2>Discover millions of movies...</h2>
+            <div className="card-width card text-bg-dark">
+                <img src={bg} className="card-img" alt="Background Picture"/>
+                    <div className="card-img-overlay header1">
+                        <h2 className="card-title title1">Welcome.</h2>
+                        <h4 className="card-text pb-2 title2">Discover millions of movies...</h4>
+
+                        <form className="d-flex  w-50 mx-5" role="search">
+                            <input className="form-control me-3 searchbar" type="search" placeholder="Start searching..." aria-label="Search"/>
+
+                            <button className="btn btn-primary" type="submit">
+                                <i className="fa-solid fa-magnifying-glass"></i>
+                            </button>
+
+                        </form>
+                    </div>
+
+            </div>
         </div>
     )
 }
+
+
+
