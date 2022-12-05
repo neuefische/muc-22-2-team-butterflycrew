3445f4d6e92c40da2a807b069713f77c68167ab6
MoinMarcell <marcell.dechant@protonmail.com>
2022-12-05 18:39:34 +0100

    Update PopularMovieComponent

--- frontend/src/MovieGallery/component/PopularMovieComponent.tsx
+++ frontend/src/MovieGallery/component/PopularMovieComponent.tsx
@@ -10,22 +10,17 @@ type PopularMovieComponentProps = {
 export default function PopularMovieComponent(props: PopularMovieComponentProps){
 
     const navigate = useNavigate()
-    function handleOnClick(){
+    function handleDetailsClick(){
         navigate("/details/" + props.popularMovie.id)
     }
 
     return(
-        <div className="card h-auto">
-            <img src={getImage(props.popularMovie.poster_path)} className="card-img-top" alt={props.popularMovie.title} />
-            <div className="card-body">
-                <h5 className="card-title">{props.popularMovie.title}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{props.popularMovie.popularity}</h6>
-                <p className="card-text">
-                    <button className={"btn btn-info mx-2"} onClick={handleOnClick}>Details</button>
-                </p>
-            </div>
-            <div className="card-footer">
-                <small className="text-muted">Genres:{props.popularMovie.genre_ids}</small>
+        <div className="col">
+            <div className="card border-0 popularMovieCard" onClick={handleDetailsClick}>
+                <img src={getImage(props.popularMovie.poster_path)} className="card-img-top rounded-start rounded-end" alt={props.popularMovie.title + " Poster"} />
+                <div className={"card-img-overlay popularMovieOverlay"}>
+                    <button className={"btn btn-info disabled"}>Popularity: {props.popularMovie.popularity}</button>
+                </div>
             </div>
         </div>
     )
