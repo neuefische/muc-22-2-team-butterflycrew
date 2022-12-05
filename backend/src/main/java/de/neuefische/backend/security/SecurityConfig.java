d6bb4b1fc1f463a7fe147eb1291757b38964929c
devBK84 <bk@kharashfeh.com>
2022-12-05 15:06:36 +0100

    Add dependency, Update SecurityConfig

--- backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -1,8 +1,9 @@
 package de.neuefische.backend.security;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,7 +12,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new Argon2PasswordEncoder();
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
---
+++ backend/src/main/java/de/neuefische/backend/security/UserController.java
@@ -0,0 +1,17 @@
+package de.neuefische.backend.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    @GetMapping("me")
+    public String helloMe(Principal principal){
+        return "Hallo" + principal.getName();
+    }
+}
