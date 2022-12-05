707d4906c7cc714370218696c212e8028b2522fe
MoinMarcell <marcell.dechant@protonmail.com>
2022-12-05 18:30:53 +0100

    Updated TopRatedMovieComponent

--- frontend/src/MovieGallery/component/TopRatedMovieComponent.tsx
+++ frontend/src/MovieGallery/component/TopRatedMovieComponent.tsx
@@ -10,22 +10,17 @@ type TopRatedMovieComponentProps = {
 export default function TopRatedMovieComponent(props: TopRatedMovieComponentProps){
 
     const navigate = useNavigate()
-    function handleOnClick(){
+    function handleDetailsClick(){
         navigate("/details/" + props.topRatedMovie.id)
     }
 
     return(
-        <div className="card h-auto">
-            <img src={getImage(props.topRatedMovie.poster_path)} className="card-img-top" alt={props.topRatedMovie.title} />
-            <div className="card-body">
-                <h5 className="card-title">{props.topRatedMovie.title}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{props.topRatedMovie.vote_average}</h6>
-                <p className="card-text">
-                    <button className={"btn btn-info mx-2"} onClick={handleOnClick}>Details</button>
-                </p>
-            </div>
-            <div className="card-footer">
-                <small className="text-muted">Genres:{props.topRatedMovie.genre_ids}</small>
+        <div className="col">
+            <div className="card border-0 topRatedMovieCard" onClick={handleDetailsClick}>
+                <img src={getImage(props.topRatedMovie.poster_path)} className="card-img-top rounded-start rounded-end" alt={props.topRatedMovie.title + " Poster"} />
+                <div className={"card-img-overlay topRatedMovieOverlay"}>
+                    <button className={"btn btn-info disabled"}>Rating: {props.topRatedMovie.vote_average}</button>
+                </div>
             </div>
         </div>
     )
