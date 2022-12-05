769206f712075ce9d0230641ae1b24f8985c6179
Marcus <marcus.schweizer91@gmail.com>
2022-12-05 20:45:48 +0100

    Add MovieExchangeGallery.tsx

---
+++ frontend/src/MovieGallery/component/MovieExchangeGallery.tsx
@@ -0,0 +1,19 @@
+import {MovieToExchange} from "../model/MovieToExchange";
+import MovieExchangeCard from "./MovieExchangeCard";
+
+export type MovieExchangeGalleryProps={
+    movies: MovieToExchange[]
+}
+
+export default function MovieExchangeGallery(props:MovieExchangeGalleryProps){
+
+    const toDoItemComponents = props.movies.map(movieData =>{
+        return <MovieExchangeCard movieToExchange={movieData} key={movieData.id}/>
+    })
+
+    return(
+        <div>
+            {toDoItemComponents}
+        </div>
+    )
+}
