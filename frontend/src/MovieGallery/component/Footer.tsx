fb4ebdfc6979fab302a1fd29cc26b76a327c7052
Marcus <marcus.schweizer91@gmail.com>
2022-12-06 19:21:37 +0100

    Update Footer <p>Have a break, have a Movie.</p>

--- frontend/src/MovieGallery/component/Footer.tsx
+++ frontend/src/MovieGallery/component/Footer.tsx
@@ -5,27 +5,24 @@ import logo1 from "../img/1.png"
 export default function Footer() {
 
     return (
-        <nav>
-            <head>
-                <meta name={"viewport"} content={"width=device-width, initial-scale=0.6"}/>
-                <title>Footer with Animation</title>
-                <link rel={"stylesheet"} href={"footer.css"}/>
-            </head>
-            <footer>
+
+
+        <footer>
+            <div className={"container-fluid justify-content-around"}>
                 <div className={"row"}>
                     <div className={"col"}>
-                        <img src={logo1} alt={"logo"}/>
-                        <p>Have a break, have a Movie.</p>
+                        <img className={"footer-logo"} src={logo1} alt={"logo"}/>
+
                     </div>
 
                     <div className={"col"}>
                         <h3>Office <div className={"underline"}><span></span></div></h3>
                         <p> XXL Road</p>
                         <p>Jùpiterfield, Mars</p>
-                        <p>blablabla, ZIP
+                        <p>blablabla, ZIP <br/>
                             54584 Space</p>
                         <p className={"email-id"}>mmvs@space.com</p>
-                        <h4>+49 - 9846546</h4>
+                        <h5>+49 - 9846546</h5>
                     </div>
                     <div className={"col"}>
                         <h3>Links <div className={"underline"}><span></span></div></h3>
@@ -44,15 +41,19 @@ export default function Footer() {
                             <li><a href={""}>Contact Us</a></li>
                         </ul>
                     </div>
-                <div className={"col social-icons"}>
-                    <i className="fa-brands fa-facebook"></i>
-                    <i className="fa-brands fa-instagram"></i>
-                    <i className="fa-brands fa-twitter"></i>
-                    <i className="fa-brands fa-youtube"></i>
+                    <div className={"col social-icons"}>
+                        <h3>Social-Media <div className={"underline"}><span></span></div></h3>
+                        <ul>
+                            <li><i className="fa-brands fa-facebook"></i></li>
+                            <li><i className="fa-brands fa-instagram"></i></li>
+                            <li><i className="fa-brands fa-twitter"></i></li>
+                            <li><i className="fa-brands fa-youtube"></i></li>
+                        </ul>
+                    </div>
                 </div>
-                </div>
-            </footer>
-        </nav>
+            </div>
+        </footer>
+
 
     )
 }
