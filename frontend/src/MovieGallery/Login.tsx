8a59e440cc7f9fdacfadee908e241792e0b5e615
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-06 10:34:32 +0100

    Update Login styling

--- frontend/src/MovieGallery/Login.tsx
+++ frontend/src/MovieGallery/Login.tsx
@@ -24,17 +24,24 @@ export default function Login(){
             .then(console.log)
     }
 
+    const loginStyle = {
+        width: "200px",
+        height: "500px"
+    }
+
     return(
         <div className={"container-sm mt-2"}>
-            <form onSubmit={onLoginSubmit}>
-                <div className="input-group mb-3">
-                    <input type="text" value={username} onChange={onUsernameChange} className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
-                </div>
-                <div className="input-group mb-3">
-                    <input type="password" value={password} onChange={onPasswordChange} className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" />
-                </div>
-                <button className={"btn btn-info"} type={"submit"}>Login</button>
-            </form>
+            <div style={loginStyle}>
+                <form onSubmit={onLoginSubmit}>
+                    <div className="input-group mb-3">
+                        <input type="text" value={username} onChange={onUsernameChange} className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                    </div>
+                    <div className="input-group mb-3">
+                        <input type="password" value={password} onChange={onPasswordChange} className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1" />
+                    </div>
+                    <button className={"btn btn-info"} type={"submit"}>Login</button>
+                </form>
+            </div>
         </div>
     )
 }
