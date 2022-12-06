aec1210bfa2cc30397362a5c54befedd83ac3c92
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-06 12:30:15 +0100

    Update UserController & add logout endpoint

--- backend/src/main/java/de/neuefische/backend/security/UserController.java
+++ backend/src/main/java/de/neuefische/backend/security/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @RestController
@@ -17,11 +18,18 @@ public class UserController {
         if(principal != null){
             return principal.getName();
         }
-        return "Mousse au chocolat!";
+        return "Mousse au chocolate!";
     }
 
     @PostMapping("login")
     public String login(){
         return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    @PostMapping("logout")
+    public String logout(HttpSession httpSession){
+        httpSession.invalidate();
+        SecurityContextHolder.clearContext();
+        return "Mousse au chocolate!";
     }
 }
