d143c3797e262602ec828350bac62d794ad4aa4f
Marcell <84642104+MoinMarcell@users.noreply.github.com>
2022-12-08 15:47:08 +0100

    Fix not shown all movies on empty searchbar

--- frontend/src/MovieGallery/component/SearchBar.tsx
+++ frontend/src/MovieGallery/component/SearchBar.tsx
@@ -1,4 +1,4 @@
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent} from "react";
 
 type SearchBarProps = {
     handleSearchText: (searchText: string) => void
@@ -6,17 +6,14 @@ type SearchBarProps = {
 
 export default function SearchBar(props: SearchBarProps){
 
-    const [searchText, setSearchText] = useState<string>("")
-
     function handleOnChangeSearchText(event: ChangeEvent<HTMLInputElement>){
-        setSearchText(event.target.value)
-        props.handleSearchText(searchText)
+        props.handleSearchText(event.target.value)
     }
 
     return(
         <div className="input-group mb-3">
             <input type="text" className="form-control" placeholder="Search for a cool Movie here..." aria-label="Search"
-                   aria-describedby="basic-addon1" onChange={handleOnChangeSearchText} value={searchText} />
+                   aria-describedby="basic-addon1" onChange={handleOnChangeSearchText} />
             <span className="input-group-text" id="basic-addon1"><i className="fa-solid fa-magnifying-glass"></i></span>
         </div>
     )
