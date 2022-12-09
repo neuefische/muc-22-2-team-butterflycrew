f872a95b1f2a906811d4e95e41bcc762d623d23d
devBK84 <bk@kharashfeh.com>
2022-12-09 15:25:59 +0100

    Update MongoUserDetailsServiceTest

--- backend/src/test/java/de/neuefische/backend/security/MongoUserDetailsServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/security/MongoUserDetailsServiceTest.java
@@ -2,26 +2,44 @@ package de.neuefische.backend.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.security.core.userdetails.UserDetails;
-
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 class MongoUserDetailsServiceTest {
 MongoUserRepository mongoUserRepository = mock(MongoUserRepository.class);
 
 MongoUserDetailsService mongoUserDetailsService = new MongoUserDetailsService(mongoUserRepository);
-//    @Test
-//    void loadUserByUsername() {
-//
-//        // GIVEN
-//        MongoUser expected = new MongoUser("1", "Frank","123");
-//        String username = "Frank";
-//        mongoUserRepository.save(expected);
-//
-//        // THEN
-//        UserDetails actual = mongoUserDetailsService.loadUserByUsername(username);
-//
-//        // WHEN
-//        assertEquals(expected, actual);
-//    }
+    @Test
+    void loadUserByUsernameExpectUsernameNotFound() {
+
+        // GIVEN
+
+        String username = "Frank";
+
+
+        // THEN
+        when(mongoUserRepository.findByUsername(username)).thenReturn(Optional.empty());
+
+        // WHEN
+        assertThrows(UsernameNotFoundException.class,()->mongoUserDetailsService.loadUserByUsername(username));
+    }
+
+    @Test
+    void loadUserByUsernameExpectUsername(){
+
+        // GIVEN
+        MongoUser expacted = new MongoUser("123", "Frank", "ABCDE");
+        String username = "Frank";
+
+        // WHEN
+        when(mongoUserRepository.findByUsername(username)).thenReturn(Optional.of(expacted));
+        UserDetails user = mongoUserDetailsService.loadUserByUsername(username);
+        // THEN
+
+        assertEquals(expacted.username(),user.getUsername());
+    }
 }
