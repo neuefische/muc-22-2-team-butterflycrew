5e3fd9f0105d4de1c92957ff0b6e47287719e5f7
devBK84 <bk@kharashfeh.com>
2022-12-09 12:04:46 +0100

    Update csrfConfig

--- backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package de.neuefische.backend.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -17,8 +19,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf().disable()
-                .httpBasic().and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .httpBasic()
+                .authenticationEntryPoint(((request, response, authException) -> response
+                        .sendError(HttpStatus.UNAUTHORIZED
+                                .value(),HttpStatus
+                                .UNAUTHORIZED
+                                .getReasonPhrase())))
+                .and()
                 .authorizeRequests()
                 .antMatchers("/api/exchange").authenticated()
                 .antMatchers("/**").permitAll()
